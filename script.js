78bf444e2f51da4154d4a61ab99cbc0c8aa4a099
Randy Lubin <randylubin@gmail.com>
2013-08-31 21:38:45 -0700

    Room Secection and full angular
    -added room selection as index
    -converted everything to angular
    -converted setInterval to $timeout

--- script.js
+++ script.js
@@ -12,39 +12,183 @@ jamSync = angular.module('jamSync', ['firebase']).
 	}).*/
 	config(function($routeProvider) {
 		$routeProvider.
-			when('/', {controller:'SyncCtrl', templateUrl:'views/sync.html'}).
+			when('/', {controller:'roomPickerCtrl', templateUrl:'views/roomPicker.html'}).
 			when('/room/:room', {controller:'SyncCtrl', templateUrl:'views/sync.html'}).
 			when('/404', {controller:'SyncCtrl', templateUrl:'views/404.html'}).
-			when('/room-select/', {controller:'roomPickerCtrl', templateUrl:'views/roomPicker.html'}).
 			otherwise({redirectTo:'/'});
 	});
 
+jamSync.controller('SyncCtrl', ['$scope', '$timeout', '$routeParams', 'angularFire',
+	function SyncCtrl($scope, $timeout, $routeParams, angularFire){
 
-jamSync.controller('SyncCtrl', ['$scope', '$routeParams', 'angularFire',
-	function SyncCtrl($scope, $routeParams, angularFire){
-		var url = 'https://jam-sync.firebaseio.com/jamInfo';
+		justEntered = true;
+
+		$scope.jamInfo = {
+			bpm: 90,
+			playStatus: 'btn-warning',
+			playing: false,
+			startTime: 99999999999,
+			disconnected: true
+		};
+
+		var url = 'https://jam-sync.firebaseio.com/' + $routeParams.room;
 		var promise = angularFire(url, $scope, 'jamInfo', {});
 
 		console.log('room number:', $routeParams.room);
 
 		$scope.bpmOptions = [60, 90, 120, 150, 180];
-		$scope.chordProgression = ['E', 'B', 'C#', 'A'];
+		$scope.chordProgression = [
+				{name: 'E',
+				status: 'active'},
+				{name: 'B',
+				status: 'inactive'},
+				{name: 'C#',
+				status: 'inactive'},
+				{name: 'A',
+				status: 'inactive'}
+			];
+
+		$scope.metronomeLight = 'off';
+
+		var offsetRef = new Firebase("https://jam-sync.firebaseio.com/.info/serverTimeOffset");
+		offsetRef.on("value", function(snap) {
+			offset = snap.val();
+			console.log('clock skew', offset);
+			estimatedServerTimeMs = new Date().getTime() + offset;
+		});
+
+		// basic setup
+
+
+		upperTimeSignature = 4;
+		lowerTimeSignature = 4;
+		frequency = (60 / 90) * 1000; //in ms
+		currentChordNumber = 0;
+		beatInMeasure = -1;
+		var offset;
+
+
+		// upon angularFire response:
 
 		promise.then(function(){
 			console.log('from fb', $scope.jamInfo);
+
+			// add defaults, if none
+			if (!$scope.jamInfo.bpm){
+				$scope.jamInfo = {
+					bpm: 90,
+					playStatus: 'btn-primary',
+					playing: false,
+					startTime: 9378000726393,
+					disconnected: false
+				};
+			}
+
+
 			$scope.$watch('jamInfo.bpm', function(){
 				frequency = (60 / $scope.jamInfo.bpm) * 1000;
 				console.log('new bpm', $scope.jamInfo.bpm);
 			});
-			//$scope.jamInfo = angular.copy($scope.remote);
-			//$scope.bpm = $scope.jamInfo.bpm;
+
+			checkStart = function(){
+				if (justEntered) {
+					return;
+				}
+				var d = new Date();
+
+				if ($scope.jamInfo.playing === true){
+					if ((d.getTime() + offset) >= $scope.jamInfo.startTime) {
+						$scope.jamInfo.playStatus = 'btn-danger';
+						$scope.startPlaying = $timeout($scope.moveOn, frequency);
+						$timeout.cancel(checkIfStarted);
+						checkIfStarted = false;
+					} else {
+						$scope.jamInfo.playStatus = 'btn-warning';
+						$scope.$apply(); //TODO: refactor this
+						$timeout(checkStart, 50);
+					}
+				} else {
+					console.log ('still checking');
+					$timeout(checkStart, 50);
+				}
+			};
+
+			$scope.moveOn = function(){
+				console.log('$scope.moveOn');
+				var d = new Date();
+				if ($scope.jamInfo.playing === true){
+					if (d.getTime() + offset >= $scope.jamInfo.startTime) {
+						
+						// advance beats and chords
+						if (beatInMeasure === upperTimeSignature - 1) {
+							currentChordNumber = $scope.nextChord(currentChordNumber);
+						}
+						beatInMeasure = (beatInMeasure + 1) % upperTimeSignature;
+
+						//flash metronome
+						$scope.metronomeLight = 'flashOn';
+						$timeout(function(){$scope.metronomeLight = 'off';}, 100);
+						$scope.$apply(); //TODO: refactor this
+						
+					} else {
+						$scope.jamInfo.playStatus = 'btn-warning';
+						$scope.$apply(); //TODO: refactor this
+					}
+					$scope.startPlaying = $timeout($scope.moveOn, frequency);
+				} else {
+					$scope.turnStuffOff();
+				}
+			};
+
+			justEntered = false;
+			checkIfStarted = $timeout(checkStart, 50);
+
+
+			$scope.togglePlay = function(){
+				if ($scope.jamInfo.playing) {
+					$scope.turnStuffOff();
+				} else {
+					var d = new Date();
+					$scope.jamInfo.startTime = d.getTime() + 3000 - offset;
+					$scope.jamInfo.playing = true;
+				}
+			};
+
+
+			$scope.turnStuffOff = function(){
+				
+				$timeout.cancel(checkIfStarted);
+				$scope.jamInfo.playing = false;
+				$scope.jamInfo.playStatus = 'btn-primary';
+				$scope.toggleChord(currentChordNumber);
+				$scope.toggleChord(0);
+				currentChordNumber = 0;
+				beatInMeasure = -1;
+				
+				
+				$timeout.cancel($scope.startPlaying);
+				checkIfStarted = $timeout(checkStart, 50);
+				console.log('turning stuff off');
+			};
+
+			$scope.toggleChord = function(chordNumber){
+				$scope.chordProgression[chordNumber].status = ($scope.chordProgression[chordNumber].status =='active') ? 'inactive' : 'active';
+			};
+
+
+			$scope.nextChord = function(chordNumber){
+				$scope.toggleChord(chordNumber);
+				chordNumber = ((chordNumber + 1) % upperTimeSignature);
+				$scope.toggleChord(chordNumber);
+				return chordNumber;
+			};
+
 		});
 	}
 ]);
 
-jamSync.controller('roomPickerCtrl', ['$scope', '$location', 'angularFire',
-	function roomPickerCtrl($scope, $location, angularFire){
-		
+jamSync.controller('roomPickerCtrl', ['$scope', '$timeout', '$location', 'angularFire',
+	function roomPickerCtrl($scope, $timeout, $location, angularFire){
 		$scope.roomNumber = [];
 		$scope.addNum = function(num){
 			console.log('added', num);
@@ -63,127 +207,5 @@ jamSync.controller('roomPickerCtrl', ['$scope', '$location', 'angularFire',
 	}
 ]);
 
-// Firebase
-
-var myDataRef = new Firebase('https://jam-sync.firebaseio.com/jamInfo');
-myDataRef.on('value', function(snapshot){
-	console.log('Playing', snapshot.val().playing);
-	jamInfo.playing = snapshot.val().playing;
-	jamInfo.startTime = snapshot.val().startTime;
-});
-
-// clock skew
-
-var offsetRef = new Firebase("https://jam-sync.firebaseio.com/.info/serverTimeOffset");
-offsetRef.on("value", function(snap) {
-	offset = snap.val();
-	console.log(offset);
-	estimatedServerTimeMs = new Date().getTime() + offset;
-});
-console.log(offset);
-
-// basic setup
-
-jamInfo = {
-	playing: false,
-	startTime: 9375935090405
-};
-bpm = 90;
-upperTimeSignature = 4;
-lowerTimeSignature = 4;
-frequency = (60 / bpm) * 1000; //in ms
-progression = [ 'E', 'B', 'C#', 'A'];
-currentChordNumber = -1;
-beatInMeasure = -1;
-var startPlaying;
-var checkIfStarted;
-var offset;
-
-checkStart = function(){
-	var d = new Date();
-
-	if (jamInfo.playing === true){
-		if ((d.getTime() + offset) >= jamInfo.startTime) {
-			angular.element('.play-stop').removeClass('btn-warning').addClass('btn-danger');
-			startPlaying = setInterval(moveOn, frequency);
-			clearInterval(checkIfStarted);
-			checkIfStarted = false;
-		} else {
-			angular.element('.play-stop').removeClass('btn-primary').addClass('btn-warning');
-		}
-	} else {
-		console.log ('still checking');
-	}
-};
-
-moveOn = function(){
-	console.log('moveOn');
-	var d = new Date();
-	if (jamInfo.playing === true){
-		if (d.getTime() + offset >= jamInfo.startTime) {
-			beatInMeasure = (beatInMeasure + 1) % lowerTimeSignature;
-			flashMetronome();
-			console.log('in move on', currentChordNumber);
-			if (beatInMeasure === 0) {
-				currentChordNumber = nextChord(currentChordNumber);
-			}
-			console.log('beep');
-		} else {
-			
-		}
-	} else {
-		turnStuffOff();
-	}
-};
-
-turnStuffOff = function(){
-	clearInterval(checkIfStarted);
-	jamInfo.playing = false;
-	angular.element('.play-stop').removeClass('btn-danger').removeClass('btn-warning').addClass('btn-primary');
-	currentChordNumber = -1;
-	beatInMeasure = -1;
-	activateChord(1);
-	clearInterval(startPlaying);
-	checkIfStarted = setInterval(checkStart, 50);
-	console.log('turning stuff off');
-};
-
-togglePlay = function(){
-	if (jamInfo.playing) {
-		turnStuffOff();
-	} else {
-		var d = new Date();
-		jamInfo.startTime = d.getTime() + 3000 - offset;
-		myDataRef.update({startTime: jamInfo.startTime});
-		jamInfo.playing = true;
-	}
-
-	myDataRef.update({playing: jamInfo.playing});
-	
-};
-
-flashMetronome = function(){
-	angular.element('.metronome').removeClass('off').addClass('flashOn');
-	setTimeout(function(){angular.element('.metronome').removeClass('flashOn').addClass('off');}, 100);
-};
-
-nextChord = function(chordNumber){
-	console.log('in next chord', chordNumber);
-	//deactivateChord(chordNumber + 1);
-	chordNumber = ((chordNumber + 1) % 4);
-	activateChord(chordNumber + 1);
-	return chordNumber;
-};
-
-activateChord = function(chordNumber){
-	angular.element('.chord-viewer > span.active').removeClass('active').addClass('inactive');
-	angular.element('.chord-viewer > span:nth-child('+ chordNumber +')').addClass('active').removeClass('inactive');
-};
-
-deactivateChord = function(chordNumber){
-	angular.element('.chord-viewer > span:nth-child('+ chordNumber +')').removeClass('active').addClass('inactive');
-};
 
 
-
-checkIfStarted = setInterval(checkStart, 50);
